c0045980d45a8c450ba65cabc85c3ce628a597e5
kazuto0291 <zutozuto03170291@gmail.com>
2020-12-26 09:53:31 +0900

    コンポーネント、グローバル登録、ローカル登録

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
-  <p>いいね({{number}}) </p>
+  <div>
+    <LikeHeader></LikeHeader>
+  <LikeNumber ></LikeNumber>
+  </div>
 </template>
 
 <script>
+import LikeHeader from  "./LikeHeader.vue"
+
   export default {
-      data() {
-        return {
-          number:5
-        }
+    components: {
+      LikeHeader: LikeHeader
     }
 
   }
---
+++ src/LikeNumber.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <P>いいね({{ number}}) </P>
+  <button @click="increment">+1</button>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      number: 5
+    };
+  },
+  methods: {
+    increment() {
+      this.number += 1;
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
