1214486f746a540c7b19c03e48863ac4ab01502d
kazuto0291 <zutozuto03170291@gmail.com>
2020-12-29 18:42:05 +0900

    router-viewにはトランジションをつけられる

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <router-view name="header"></router-view>
-    <router-view></router-view>
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
     <div class="main">
       <button @click="myAnimation = 'slide'">Slide</button>
       <button @click="myAnimation = 'fade'">Fade</button>
